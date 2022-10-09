5f65e5399e63ca1b2b4f3aebef0dc999a9db86c3
Yershat Kuanysh <yershat.kuanysh@gmail.com>
2022-10-08 22:33:15 -0700

    7 kyu

---
+++ Code_wars_7_kyu/series_sum.js
@@ -0,0 +1,37 @@
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n){
+    if(n===0){
+        return "0.00";
+    }
+    else if(n>0){
+    l = [];
+    a = 1;
+    b = 1;
+    for(let i = 1; i < n;i++){
+        l.push(a+3);
+        a +=3;
+    }
+    for(let i = 0 ; i < l.length; i++){
+        b = b + (1/l[i]);  
+        
+    }
+      
+    return parseFloat(b).toFixed(2);
+    }}
+
+
